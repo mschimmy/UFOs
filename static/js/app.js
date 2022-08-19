e346fa97b47da2e6760db9601b746d6bf8d6fab7
Madison Schimmel <mschimmel@wireless-10-147-1-135.public.utexas.edu>
2022-08-19 16:57:38 -0500

    added filters using D3.

--- static/js/app.js
+++ static/js/app.js
@@ -24,4 +24,25 @@ function buildTable(data) {
     });
 }
 
- 
+//11.5.3 create a function to add a button that will filter the data by date
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    //11.5.4 check to see if a date was entered and filter the data using that date
+    if (date) {
+
+        // apply "filter" to the table data to only keep the rows where the "datetime" value matches the filter value
+        filtredData = filteredData.filter(row => row.datetime === date);
+    }
+
+    //11.5.4 rebuild the table using the filtered data
+    // NOTE: if no date was entered, then filteredData will just be the original tableData
+    buildTable(filteredData);
+}
+
+//11.5.4 attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+//11.5.4 build the table when the page loads
+buildTable(tableData);
